a5c1087633ba3e3f9af38fe7b3dbe4954cb58dba
Kakasei <Kakasei@qq.com>
2025-11-14 15:46:05 +0800

    每日一题打卡

---
+++ leetcode/2536子矩阵元素加1/2536子矩阵元素加1.ts
@@ -0,0 +1,18 @@
+// 暴力法
+function rangeAddQueries(n: number, queries: number[][]): number[][] {
+  // 初始化
+  const matrix: number[][] = new Array(n);
+  for (let i = 0; i < n; i++) {
+    matrix[i] = new Array(n).fill(0);
+  }
+
+  for (const [r1, c1, r2, c2] of queries) {
+    for (let r = r1; r <= r2; r++) {
+      for (let c = c1; c <= c2; c++) {
+        matrix[r][c]++;
+      }
+    }
+  }
+
+  return matrix
+}
