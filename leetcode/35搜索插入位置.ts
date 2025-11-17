a119eaf76665427b4d9469d4e6bec2e5b03b0675
Kakasei <Kakasei@qq.com>
2025-11-17 21:14:04 +0800

    完成35

---
+++ leetcode/35搜索插入位置.ts
@@ -0,0 +1,16 @@
+// 二分查找元素插入位置，例题
+function searchInsert(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] >= target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+}
