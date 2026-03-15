f81f93e84ec4b04f219e98b766d52b0d48fbfa66
Kakasei <CitrusKakasei@outlook.com>
2026-03-15 22:36:39 +0800

    同步

---
+++ leetcode/1-1000/1-100/70-爬楼梯/参考答案-递推.ts
@@ -0,0 +1,12 @@
+function climbStairs(n: number): number {
+    if (n === 1) {
+        return 1;
+    }
+    const dp = new Array(n);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
---
+++ leetcode/1-1000/201-300/215-数组中的第K个最大元素/mikansei.ts
@@ -0,0 +1,8 @@
+function findKthLargest(nums: number[], k: number): number {
+    const stack:number[]=[]
+    for(const n of nums){
+        if(stack.length<k){
+            
+        }
+    }
+};
