94118d695af90721de9e590426c0a597c1b6c807
Kakasei <Kakasei@qq.com>
2026-04-06 23:10:01 +0800

    2刷lc-1

--- leetcode/1-1000/1-100/1-两数之和/参考答案.ts
+++ leetcode/1-1000/1-100/1-两数之和/参考答案.ts
@@ -1,10 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
     const n = nums.length;
-    const map = new Map<number, number>();
+    const map = new Map();
     for (let i = 0; i < n; i++) {
         if (map.has(target - nums[i])) {
-            return [map.get(target - nums[i])! , i];
+            return [map.get(target - nums[i]), i];
+        } else {
+            map.set(nums[i], i);
         }
-        map.set(nums[i], i);
     }
 }
